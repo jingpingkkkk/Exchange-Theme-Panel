123d6bfd220444ff22c125d49f34b74a3faa9072
Shilpa-Hoch <137758395+Shilpa-Hoch@users.noreply.github.com>
2023-11-02 15:33:20 +0530

    fix editor issue

--- src/Pages/Promotion/PromotionForm/CustomEditor.js
+++ src/Pages/Promotion/PromotionForm/CustomEditor.js
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
-import { Editor } from 'react-draft-wysiwyg';
-import { EditorState, ContentState, convertFromHTML } from 'draft-js';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import React, { useEffect, useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, ContentState, convertFromHTML } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 function CustomEditor({ initialValue, onEditorStateChange }) {
+  const [editorState, setEditorState] = useState();
+
+  useEffect(() => {
     const blocksFromHTML = convertFromHTML(initialValue);
     const contentState = ContentState.createFromBlockArray(
-        blocksFromHTML.contentBlocks,
-        blocksFromHTML.entityMap
+      blocksFromHTML.contentBlocks,
+      blocksFromHTML.entityMap
     );
+    const newEditorState = EditorState.createWithContent(contentState);
+    setEditorState(newEditorState);
+  }, [initialValue]);
 
-    const [editorState, setEditorState] = useState(
-        EditorState.createWithContent(contentState)
-    );
+  const handleEditorStateChange = (newEditorState) => {
+    setEditorState(newEditorState);
+    onEditorStateChange(newEditorState);
+  };
 
-    const handleEditorStateChange = (newEditorState) => {
-        setEditorState(newEditorState);
-        onEditorStateChange(newEditorState);
-    };
-
-    return (
-        <div>
-            <Editor
-                editorState={editorState}
-                onEditorStateChange={handleEditorStateChange}
-            />
-        </div>
-    );
+  return (
+    <Editor
+      editorState={editorState}
+      onEditorStateChange={handleEditorStateChange}
+      editorStyle={{ border: "1px solid #eaedf1" }}
+    />
+  );
 }
 
 export default CustomEditor;
